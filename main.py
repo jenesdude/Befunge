f6ebef637019757196447c93eff52ca91f000d98
Max Smagin <maximus_smagin@mail.ru>
2023-03-21 23:56:00 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+"""Interpreter for esoteric stack language Befunge"""
+
+
+
+def read_program(filename):
+    with open(filename, "r", encoding="utf-8") as 
+
+
+def evaluate(code):
+    """evaluates code of Befunge. Code is any less or equal to 25×80 text block"""
+
+
+
+
+
+def main():
+    pass
